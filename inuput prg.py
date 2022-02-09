0734e0f16c710e6ead57024b736135f2d17eaf85
SUSHMAGANDAM <99244274+SUSHMAGANDAM@users.noreply.github.com>
2022-02-09 14:34:04 +0530

    Add files via upload

---
+++ inuput prg.py
@@ -0,0 +1,15 @@
+# from cgi import print_directory
+
+
+# d=input('enter your name')
+# # u=input('enter your name')
+# if d == "bablu ":
+#     print(d*100)
+# else:
+#     print(d*1000)
+# print(d*1000)
+# print('ok','done','fine',spe='')
+# print('ok','done','fine',sep=' ')
+# print('wall','clock',end='.')
+# print('coast off it')
+i=
